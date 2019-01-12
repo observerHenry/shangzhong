7b8bd5d18d13000c1191e7506e820edcd787e9e1
haylhf <haylhf@163.com>
2019-01-12 18:28:49 +0800

    更新页面控件

--- src/components/schoolbus/busmanage.vue
+++ src/components/schoolbus/busmanage.vue
@@ -1,37 +1,53 @@
-<template xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-bind="http://www.w3.org/1999/xhtml">
-    <div>
-        BusManage
-    </div>
-</template>
+<template xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-bind="http://www.w3.org/1999/xhtml" >
+    <div >
+        <el-row>
+            <el-col>
+                <span :span="2" >校车列表</span >
+                <el-button
+						:offset="18"
+		                :span="2"
+		                icon="el-icon-download"
+		                size="normal"
+		                type="primary"
+		                style="text-align: right"
+		                @click="search" >导入数据
+                </el-button >
+	            <el-button
+			            :offset="1"
+			            :span="2"
+			            icon="el-icon-upload2"
+			            size="normal"
+			            type="primary"
+			            style="text-align: right"
+			            @click="search">上传数据
+	            </el-button>
+            </el-col >
+        </el-row >
+    </div >
+</template >
 
-<script>
+<script >
     var _this;
     export default {
-        name: "BusManage",
-        components: {},
-        data () {
-            _this = this;
-            return {
+	    name: "BusManage",
+	    components: {},
+	    data () {
+		    _this = this;
+		    return {}
+	    },
+	    methods: {},
+	    computed: {},
 
-            }
-        },
-        methods: {
+	    filters: {},
 
-        },
-        computed: {
-
-        },
-
-        filters: {},
-
-        created: function () {
+	    created: function () {
 
 
-        },
-        mounted: function () {
-        }
+	    },
+	    mounted: function () {
+	    }
     }
-</script>
-<style>
+</script >
+<style >
 
-</style>
+</style >
